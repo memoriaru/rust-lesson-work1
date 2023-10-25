bcb69c5322fcfaa1ecca95d066a7f3250862b9e7
memoriaru <79190669+memoriaru@users.noreply.github.com>
2023-10-25 19:35:16 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,26 @@
 pub mod mod1 {
     pub fn mod1_print() {
-        for idx in (65..=122).rev() {
-            println!("{}", char::from_u32(idx).unwrap())
+        println!("sub mod1");
+        for idx in 65..=122 {
+            if idx > 64 && idx < 91 {
+                println!("{}", char::from_u32(idx).unwrap().to_ascii_lowercase());
+            } else if idx > 96 && idx < 123 {
+                println!("{}", char::from_u32(idx).unwrap().to_ascii_uppercase());
+            } else {
+                println!("{}", char::from_u32(idx).unwrap())
+            }
         }
     }
 
     pub mod mod2 {
         pub fn mod2_print() {
+            println!("sub mod2");
             for idx in 65..=122 {
                 println!("{}", char::from_u32(idx).unwrap())
             }
         }
     }
 }
-
 
 fn main() {
     use mod1::mod1_print;
